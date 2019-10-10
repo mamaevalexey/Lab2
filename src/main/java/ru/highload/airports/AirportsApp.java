41938defdc0b69209547005ac249c08f6ab6ba36
aleksma <aleksma99@yandex.ru>
2019-10-10 22:18:55 +0300

    Scripted auto-commit on change (2019-10-10 22:18:55) by gitwatch.sh

--- src/main/java/ru/highload/airports/AirportsApp.java
+++ src/main/java/ru/highload/airports/AirportsApp.java
@@ -2,22 +2,34 @@ package ru.highload.airports;
 
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import java.io.IOException;
 
 public class AirportsApp {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
         Job job = Job.getInstance();
-        job.setJarByClass(JoinJob.class);
-        job.setJobName("JoinJob sort");
+        job.setJarByClass(AirportsApp.class);
+        job.setJobName("Airports JoinJob");
+
+        // FIXME:
         MultipleInputs.addInputPath(job, new Path(args[0]), TextInputFormat.class, CallsJoinMapper.class);
         MultipleInputs.addInputPath(job, new Path(args[1]), TextInputFormat.class, SystemsJoinMapper.class);
         FileOutputFormat.setOutputPath(job, new Path(args[2]));
+
+        // FIXME:
         job.setPartitionerClass(TextPair.FirstPartitioner.class);
         job.setGroupingComparatorClass(TextPair.FirstComparator.class);
         job.setReducerClass(JoinReducer.class);
         job.setMapOutputKeyClass(TextPair.class);
+        
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(Text.class);
         job.setNumReduceTasks(2);
+
         System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
