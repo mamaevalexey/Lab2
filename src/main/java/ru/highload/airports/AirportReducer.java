1ab01029164fc2bb4f387aecdcb4f91ab3529f33
aleksma <aleksma99@yandex.ru>
2019-10-12 01:07:13 +0300

    Scripted auto-commit on change (2019-10-12 01:07:13) by gitwatch.sh

--- src/main/java/ru/highload/airports/AirportReducer.java
+++ src/main/java/ru/highload/airports/AirportReducer.java
@@ -4,12 +4,16 @@ package ru.highload.airports;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
+import java.io.IOException;
+import java.util.Iterator;
+
 public class AirportReducer extends Reducer<AirportIDWritableComparable, Text, Text, Text> {
 
     @Override
-    protected void reduce(TextPair key, Iterable<Text> values, Context context) throws
+    protected void reduce(AirportIDWritableComparable key, Iterable<Text> values, Context context) throws
             IOException, InterruptedException {
         Iterator<Text> iter = values.iterator();
+        
         Text systemInfo = new Text(iter.next());
         while (iter.hasNext()) {
             Text call = iter.next();
