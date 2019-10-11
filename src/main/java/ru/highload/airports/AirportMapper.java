4b381e41a175e264a8a8ce1aef6cc49127f6d7da
aleksma <aleksma99@yandex.ru>
2019-10-12 00:41:48 +0300

    Scripted auto-commit on change (2019-10-12 00:41:48) by gitwatch.sh

--- src/main/java/ru/highload/airports/AirportMapper.java
+++ src/main/java/ru/highload/airports/AirportMapper.java
@@ -7,6 +7,6 @@ import org.apache.hadoop.mapreduce.Mapper;
 public class AirportMapper extends Mapper<LongWritable, Text, AirportWritable, Text> {
     @Override
     protected void map(LongWritable key, Text value, Context context){
-        
+        AirportWritable airport = new AirportWritable(value);
     }
 }
