19956f27647951fb9086824753714fb5bb4eed86
aleksma <aleksma99@yandex.ru>
2019-10-12 00:24:31 +0300

    Scripted auto-commit on change (2019-10-12 00:24:31) by gitwatch.sh

--- src/main/java/ru/highload/airports/AirportIDWritable.java
+++ src/main/java/ru/highload/airports/AirportIDWritable.java
@@ -1,12 +1,18 @@
 package ru.highload.airports;
 
 import org.apache.hadoop.io.Writable;
+import org.apache.hadoop.io.WritableComparable;
 
 import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
 
-public class AirportIDWritable implements Writable {
+public class AirportIDWritable implements WritableComparable {
+    @Override
+    public int compareTo(Object o) {
+        return 0;
+    }
+
     @Override
     public void write(DataOutput dataOutput) throws IOException {
 
