ce7370e3ba0f1b3b6cbfbd3b35e9afe99adf73d8
aleksma <aleksma99@yandex.ru>
2019-10-12 00:43:46 +0300

    Scripted auto-commit on change (2019-10-12 00:43:46) by gitwatch.sh

--- src/main/java/ru/highload/airports/AirportMapper.java
+++ src/main/java/ru/highload/airports/AirportMapper.java
@@ -4,12 +4,15 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
+import java.io.IOException;
+
 public class AirportMapper extends Mapper<LongWritable, Text, AirportWritable, Text> {
     @Override
-    protected void map(LongWritable key, Text value, Context context){
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
         AirportWritable airport = new AirportWritable(value);
 
-        if (airport.getAirportID() != -1)
+        if (airport.getAirportID() != -1) {
             context.write(airport, new Text(airport.getAirportName()));
+        }
     }
 }
