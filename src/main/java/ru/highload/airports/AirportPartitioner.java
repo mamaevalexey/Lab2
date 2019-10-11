188bd9105591161a03877df0a6afb66b377ab93e
aleksma <aleksma99@yandex.ru>
2019-10-12 01:01:09 +0300

    Scripted auto-commit on change (2019-10-12 01:01:09) by gitwatch.sh

--- src/main/java/ru/highload/airports/AirportPartitioner.java
+++ src/main/java/ru/highload/airports/AirportPartitioner.java
@@ -1,4 +1,11 @@
 package ru.highload.airports;
 
-public class AirportPartitioner {
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.lib.partition.HashPartitioner;
+
+public class AirportPartitioner extends HashPartitioner<AirportIDWritableComparable, Text> {
+    @Override
+    public int getPartition(AirportIDWritableComparable key, Text value, int numReduceTasks) {
+        return super.getPartition(key, value, numReduceTasks);
+    }
 }
