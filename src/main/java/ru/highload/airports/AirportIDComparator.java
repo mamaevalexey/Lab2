c9a7c592747c089caa4ac43dd1bf6aa00a411f02
aleksma <aleksma99@yandex.ru>
2019-10-12 00:18:01 +0300

    Scripted auto-commit on change (2019-10-12 00:18:01) by gitwatch.sh

--- src/main/java/ru/highload/airports/AirportIDComparator.java
+++ src/main/java/ru/highload/airports/AirportIDComparator.java
@@ -1,4 +1,11 @@
 package ru.highload.airports;
 
-public class AirportIDComparator {
+import org.apache.hadoop.io.WritableComparable;
+import org.apache.hadoop.io.WritableComparator;
+
+public class AirportIDComparator extends WritableComparator {
+    @Override
+    public int compare(WritableComparable a, WritableComparable b) {
+        return super.compare(a, b);
+    }
 }
