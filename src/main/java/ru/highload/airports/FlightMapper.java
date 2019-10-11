89065fad6f9c1ca0aa5b40ec4d8ca77a3d2c209c
aleksma <aleksma99@yandex.ru>
2019-10-12 00:56:20 +0300

    Scripted auto-commit on change (2019-10-12 00:56:20) by gitwatch.sh

--- src/main/java/ru/highload/airports/FlightMapper.java
+++ src/main/java/ru/highload/airports/FlightMapper.java
@@ -7,6 +7,9 @@ import org.apache.hadoop.mapreduce.Mapper;
 public class FlightMapper extends Mapper<LongWritable, Text, FlightWritable, Text> {
     @Override
     protected void map(LongWritable key, Text value, Context context){
-
+        FlightWritable flight = new FlightWritable(value);
+        if (flight.getDestAirportID() != -1 && flight.getDelayTime() != -1) {
+            context.write();
+        }
     }
 }
