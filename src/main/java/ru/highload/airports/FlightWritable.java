3ea8f4614fdcc192d374b883ec9ff945fdc1245f
aleksma <aleksma99@yandex.ru>
2019-10-11 23:56:15 +0300

    Scripted auto-commit on change (2019-10-11 23:56:15) by gitwatch.sh

--- src/main/java/ru/highload/airports/FlightWritable.java
+++ src/main/java/ru/highload/airports/FlightWritable.java
@@ -3,10 +3,21 @@ package ru.highload.airports;
 import org.apache.hadoop.io.Text;
 
 public class FlightWritable {
-    int destAirportID
-    public FlightWritable(Text text){
+    int destAirportID;
+
+    public FlightWritable(Text text) {
         String[] cols = text.toString().split(",");
         String destAirport = cols[14].replaceAll("\"", "");
-        if(destAirport == "")
+        String delay = cols[17].replaceAll("\"", "");
+        if (destAirport.equals("DEST_AIRPORT_ID")) {
+            destAirportID = -1;
+        } else {
+            destAirportID = Integer.parseInt(destAirport);
+        }
+        if (destAirport.equals("DEST_AIRPORT_ID")) {
+            destAirportID = -1;
+        } else {
+            destAirportID = Integer.parseInt(destAirport);
+        }
     }
 }
