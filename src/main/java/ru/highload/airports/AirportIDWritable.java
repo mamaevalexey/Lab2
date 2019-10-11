90b6c320dd26ef753015d214e47dc8b4349b15da
aleksma <aleksma99@yandex.ru>
2019-10-12 00:22:52 +0300

    Scripted auto-commit on change (2019-10-12 00:22:51) by gitwatch.sh

--- src/main/java/ru/highload/airports/AirportIDWritable.java
+++ src/main/java/ru/highload/airports/AirportIDWritable.java
@@ -1,4 +1,19 @@
 package ru.highload.airports;
 
-public class AirportIDWritable {
+import org.apache.hadoop.io.Writable;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+public class AirportIDWritable implements Writable {
+    @Override
+    public void write(DataOutput dataOutput) throws IOException {
+
+    }
+
+    @Override
+    public void readFields(DataInput dataInput) throws IOException {
+
+    }
 }
