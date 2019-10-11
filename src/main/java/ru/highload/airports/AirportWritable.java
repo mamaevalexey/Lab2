298721a5c05edec5d3a7ce0dd909c797057f0756
aleksma <aleksma99@yandex.ru>
2019-10-12 00:09:47 +0300

    Scripted auto-commit on change (2019-10-12 00:09:47) by gitwatch.sh

--- src/main/java/ru/highload/airports/AirportWritable.java
+++ src/main/java/ru/highload/airports/AirportWritable.java
@@ -8,42 +8,42 @@ import java.io.DataOutput;
 import java.io.IOException;
 
 public class AirportWritable implements Writable {
-    private int destAirportID;
-    private float delayTime;
+    private int airportID;
+    private String airportName;
 
     public AirportWritable(Text text) {
         String[] cols = text.toString().split(",");
         String airport = cols[0].replaceAll("\"", "");
-        String delay = cols[17].replaceAll("\"", "");
+        String name = cols[1].replaceAll("\"", "");
         if (airport.equals("Code")) {
-            destAirportID = -1;
+            airportID = -1;
         } else {
-            destAirportID = Integer.parseInt(airport);
+            airportID = Integer.parseInt(airport);
         }
-        if (delay.equals("ARR_DELAY")) {
-            delayTime = -1;
+        if (name.equals("Description")) {
+            airportName = "";
         } else {
-            delayTime = Float.parseFloat(airport);
+            airportName = name;
         }
     }
 
-    public float getDelayTime() {
-        return delayTime;
+    public String getAirportName() {
+        return airportName;
     }
 
-    public int getDestAirportID() {
-        return destAirportID;
+    public int getAirportID() {
+        return airportID;
     }
 
     @Override
     public void write(DataOutput dataOutput) throws IOException {
-        dataOutput.writeInt(destAirportID);
-        dataOutput.writeFloat(delayTime);
+        dataOutput.writeInt(airportID);
+        dataOutput.writeUTF(airportName);
     }
 
     @Override
     public void readFields(DataInput dataInput) throws IOException {
-        destAirportID = dataInput.readInt();
-        delayTime = dataInput.readFloat();
+        airportID = dataInput.readInt();
+        airportName = dataInput.readUTF();
     }
 }
